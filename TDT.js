a0ed9788b68e944b3ae3e4b1114e3f633dbd9b94
euglena1215 <teppest1215@gmail.com>
2015-10-29 22:12:55 +0900

    読みやすいようにコメントを削除

--- TDT.js
+++ TDT.js
@@ -30,15 +30,7 @@ function delay(){
 }
 
 function main(csv){
-	// setup(csv);
-	// disp(departure);
-	// setInterval('disp',1000,departure);
-	// while(1){
-			// setTimeout('disp',1000);
-			// disp(departure);
-
 	setInterval(function(){
-		console.log(csv);
 		var next_train_time = setup(csv);
 		console.log(next_train_time);
 		var departure = next_train_time.split(',');
@@ -46,93 +38,47 @@ function main(csv){
 		for(var i = 0;i < 3;i++){
 			departure[i] = Number(departure[i]);
 		}
-		console.log(departure);
-		// var departure = setup(csv_text);
-			console.log(departure);
-			disp(departure);
+		disp(departure);
 
-		  //   //再読み込み	
-		  //   if (flag === 1){
-		  //   	m++;
-				// var departure = setup(csv_text);
-		  //   	flag = 0;
-		  //   }
+	    //再読み込み	
+	    // if (flag === 1){
+	    	// m++;
+			// var departure = setup(csv_text);
+	    	// flag = 0;
+	  //   }
 	}, 1000, csv);
 
 }
 
-function repeat(csv) {
-			var next_train_time = setup(csv);
-			var departure = next_train_time.split(',');
-			console.log(departure);
-			for(var i = 0;i < 3;i++){
-				departure[i] = Number(departure[i]);
-			}
-
-		// var departure = setup(csv_text);
-			console.log(departure);
-			disp(departure);
-
-		  //   //再読み込み	
-		  //   if (flag === 1){
-		  //   	m++;
-				// var departure = setup(csv_text);
-		  //   	flag = 0;
-		  //   }
-}
-
 function setup(csv){//次回の発車時刻を特定しdepartureに代入
 
-		var arrayed_csv = csv.split('\n');
+	var arrayed_csv = csv.split('\n');
 
-		// if(flag === 0){	
+	var time = new Date();
+	var hour = time.getHours();
+	var	minute = time.getMinutes();
+	var second = time.getSeconds();
 
-			var time = new Date();
-			var hour = time.getHours();
-			var	minute = time.getMinutes();
-			var second = time.getSeconds();
+	var l = 0;
+	var min_hour,min_minute,min_second;
+	min_hour = 23;min_minute = min_second = 59;
 
-			var l = 0;
-			var min_hour,min_minute,min_second;
-			min_hour = 23;min_minute = min_second = 59;
+	for(var j = 0;j < arrayed_csv.length;j++){
+		var dif = Number(arrayed_csv[j].substring(0,2)) - hour;
 
-			for(var j = 0;j < arrayed_csv.length;j++){
-				var dif = Number(arrayed_csv[j].substring(0,2)) - hour;
-
-				if(dif < 0){//発車時刻が過ぎていたら回避
-					continue;
-				}else {
-					if(arrayed_csv[j].substring(3,5) < minute && dif == 0){
-						continue;
-					}
-					l = j;
-					break;
-				} 
+		if(dif < 0){//発車時刻が過ぎていたら回避
+			continue;
+		}else {
+			if(arrayed_csv[j].substring(3,5) < minute && dif == 0){
+				continue;
 			}
-			// for(var j = l;j < arrayed_csv.length;j++){
-			// 	var minute = time.getMinutes();
-			// 	var dif = Number(arrayed_csv[j].substring(3,5)) - minute;
-
-			// 	if((arrayed_csv[l].substring(0,2)) < (arrayed_csv[j].substring(0,2))) break;
-
-			// 	if(dif < 0){//発車時刻が過ぎていたら回避
-			// 		continue;
-			// 	}else {
-			// 		min_minute = dif;
-			// 		m = j;
-			// 		break;
-			// 	}
-			// }
-		// }
-		console.log(String(arrayed_csv[l]))
-		return String(arrayed_csv[l]);
-		// var departure = arrayed_csv[m].split(',');
-		// for(var i = 0;i < 3;i++){
-		// 	departure[i] = Number(departure[i]);
-		// }
-		// console.log(departure[0],departure[1],departure[2]);
-		// return departure[0],departure[1],departure[2];
-		
+			l = j;
+			break;
+		} 
+	}
+	
+	console.log(String(arrayed_csv[l]))
+	return String(arrayed_csv[l]);
 }
 
 function disp(departure){
@@ -158,9 +104,4 @@ function disp(departure){
 	
 		text.innerHTML = time_dif;
 		console.log(departure);
-
-	// if((Number(departure[0]) == hour) && (Number(departure[1]) == minute) && (Number(departure[2]) == second))
-	// if(hour_dif - minute_dif - second_dif)
-		// console.log()
-		// flag = 1;
 }
